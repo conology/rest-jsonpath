26f435bb7d96fdc2b7366e72ddbbdc2fd8415f84
Omar Sood <goatfryed@gmail.com>
2025-03-12 10:09:07 +0100

    test: support skipped tests for future cases in csv test csv

--- src/mongo-spring/src/test/java/io/github/conology/jsonpath/mongo/spring/MongoCriteriaCompilerPassTest.java
+++ src/mongo-spring/src/test/java/io/github/conology/jsonpath/mongo/spring/MongoCriteriaCompilerPassTest.java
@@ -1,33 +1,25 @@
 package io.github.conology.jsonpath.mongo.spring;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.opentest4j.TestAbortedException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MongoCriteriaCompilerPassTest {
 
-    @ParameterizedTest(name = "{index} {0}")
+    @ParameterizedTest(name = "[{index}] {0}")
     @CsvFileSource(resources = "/MongoCriteriaCompilerPassTest.csv", numLinesToSkip = 1)
-    void test(String input, String expected) {
+    void test(String input, String mongoQuery) {
+        switch (mongoQuery) {
+            case "!unsupported" -> throw new TestAbortedException("error testing not implemented");
+            case null -> throw new TestAbortedException("behavior not yet defined");
+            default -> testTranslatablePath(input, mongoQuery);
+        }
+    }
+
+    private void testTranslatablePath(String input, String mongoQuery) {
         var criteria = new JsonPathToCriteriaCompiler().compile(input);
-        assertEquals(expected, criteria.getCriteriaObject().toJson());
-    }
-
-    @Test
-    void manualTest2() {
-        test(
-            "store == \"The Book Haven\"",
-            "{\"owner.contacts\": {\"$elemMatch\": {\"type\": \"secondMail\", \"value\": \"alice@bookhaven.com\" }}}"
-        );
-    }
-
-    @Test
-    void manualTest() {
-        test(
-            "owner.contacts[?@.type==\"secondMail\" && @.value==\"alice@bookhaven.com\"]",
-            "{\"owner.contacts\": {\"$elemMatch\": {\"type\": \"secondMail\", \"value\": \"alice@bookhaven.com\" }}}"
-        );
+        assertEquals(mongoQuery, criteria.getCriteriaObject().toJson());
     }
 }
